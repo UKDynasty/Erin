333674b014693eece02ce2f3b96bd6c257f12fb2
Dan Abrey <danabrey@gmail.com>
2017-09-30 23:27:06 +0100

    Parse int

--- src/erin.js
+++ src/erin.js
@@ -67,7 +67,7 @@ const processMessage = (message, conversationId) => {
                 })
                 .then(espnRosterData => {
                     return espnRosterData.slots.map(playerData => {
-                        return playerData.player.firstName + " " + playerData.player.lastName + ", " + espnAPI.positionIds[playerData.player.defaultPositionId];
+                        return playerData.player.firstName + " " + playerData.player.lastName + ", " + espnAPI.positionIds[parseInt(playerData.player.defaultPositionId, 10)];
                     });
                 })
                 .then(roster => {
